2fbf7c44e9ad732be3a49b024ee1d4a6dd7203b2
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2023-09-29 12:27:38 +0700

    nan

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: "aom",
+    age: 21,
+    image:
+      "https://f.ptcdn.info/741/062/000/pnp6ur5jjfht1aVxM90n-o.jpg",
+  },
+  {
+    id: 2,
+    name: "Tinny",
+    age: 19,
+    image:
+      "https://media.tenor.com/TdluIc5YOS4AAAAM/%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%99%E0%B8%95%E0%B9%8C%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%9E-junkyardboyz.gif",
+  },
+  {
+    id: 3,
+    name: "Punsany",
+    age: 20,
+    image:
+      "https://i.makeagif.com/media/5-12-2021/2AGQY9.gif",
+  },
+  {
+    id: 4,
+    name: "Oys",
+    age: 19,
+    image:
+      "https://cdn.pillowcast.app/user/SgIE4YwpYQY7BPvKTZqHDz17HO22/1636021577764-pSgIE4YwpYQY7BPvKTZqHDz17HO22-500x500.jpg",
+  },
+  {
+    id: 5,
+    name: "4",
+    age: 19,
+    image:
+      "https://img.online-station.net/_content/2017/0321/gallery/r_14900872571.jpg",
+  },
+];
+
+export default data;
